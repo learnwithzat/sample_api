2b97f376930c2d1a5189af82ae63616c5dcafe42
Zainul Abid T <learnwithzat@gmail.com>
2026-03-29 11:11:43 +0300

    feat: added auth module

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors(); // ✅ allow all origins
-  const port = process.env.PORT ?? 5000;
-  await app.listen(port, '0.0.0.0');
-  Logger.log(
-    `🚀 Application is running on: https://sample-api-9nzo.onrender.com`,
-    'Bootstrap',
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
   );
+
+  app.enableCors();
+
+  // Render provides the PORT automatically
+  const port = process.env.PORT || 5000;
+
+  await app.listen(port, '0.0.0.0');
+
+  Logger.log(`🚀 Server running on port ${port}`, 'Bootstrap');
 }
 bootstrap();
